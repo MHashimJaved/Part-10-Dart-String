8e264624644db4d9b2e65bcb95891b8e5e2d7ee1
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-27 16:30:07 +0500

    Create main1.dart
    //this is how we got the length of string
     //this is how we get the any value of inside the string

---
+++ main1.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  
+  String age = '12';
+  String index = '12';
+  String result;
+  
+  result = age + index;
+  
+  print(age.length); //this is how we got the length of string
+  print(index. length);
+  
+  print(age[1]); //this is how we get the any value of inside the string
+  print(result);
+  
+}

8f7b716e0725cdc360d55958acd81c057e9b0687
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-27 16:56:28 +0500

    Create main3.dart
    Another example to split variables in String value:

---
+++ main3.dart
@@ -0,0 +1,13 @@
+void main() {
+  String age = 'This is hashim javed, follow on github';
+  
+  print(age);
+  print(age.toUpperCase());
+  print(age.toLowerCase());
+  
+  print(age[2].toUpperCase());
+  print(age[11].toLowerCase());  
+  
+  print(age.split(''));
+  
+}
